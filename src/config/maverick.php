c753d4e80ec2f17c00810514ab75d670a0931b7b
Travier Moorlag <moorlagt@gmail.com>
2019-08-09 00:56:35 -0400

    updated config

--- src/config/maverick.php
+++ src/config/maverick.php
@@ -8,9 +8,19 @@ return [
         Models Maverick will use to generate forms
     */ 
     'models' => [
-
+        'Lead',
+        'User',
+        'Customer'
     ],
-
-    'column_relationships' => []
+    'column_relationships' => [
+        'dealer_id' => App\Dealer::class,
+        'store_id' => App\Store::class
+    ],
+    'column_ordering' => [
+        'leads' => [
+            ['first_name', 'dealer_id'],
+            'email',
+        ]
+    ]
 ];
 ?>
