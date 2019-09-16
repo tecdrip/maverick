207e4fd02fba3f758904ff8ca9b34eb6263f55d3
Travier Moorlag <moorlagt@gmail.com>
2019-09-16 18:59:01 -0400

    Fix for password issue

--- src/views/inputs/varchar.blade.php
+++ src/views/inputs/varchar.blade.php
@@ -5,6 +5,8 @@
         <select class="form-control" name="{{ $column->Field }}">
             @include('maverick::inputs.common.states')
         </select>
+    @elseif($column->Field == "password")
+        <input autocomplete="off" class="form-control @error($column->Field) is-invalid @enderror" type="password" name="password" @if(@$column->Type['length']) maxlength='{{ $column->Type['length'] }}' @endif />
     @else
         <input class="form-control @error($column->Field) is-invalid @enderror" @if(@$column->Value) value="{{ $column->Value }}" @endif type="search" name="{{ $column->Field }}" @if(@$column->Type['length']) maxlength='{{ $column->Type['length'] }}' @endif />
     @endif
